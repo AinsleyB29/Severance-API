c9dddf57074897181249fb354111082a24968893
Ainsley Brundage <abrundage95@gmail.com>
2022-04-08 17:11:17 -0400

    changed heroku link

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 // Setting up connection for MongoDB
 const MONGODB_URI =
   process.env.PROD_MONGODB ||
-  'mongodb+srv://AinsleyB29:<thegeneralisme>@severance.azsl3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://AinsleyB29:<thegeneralisme>@severance.azsl3.mongodb.net/severance-show?retryWrites=true&w=majority';
 
 // This is for Model.findByIdAndUpdate so that we can see what we updated.
 mongoose.set('returnOriginal', false);
